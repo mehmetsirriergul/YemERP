fa9f98cd0b1247aceeb5c4e7371c3aecadded90c
Mehmet Sırrı Ergül <baycgecekahvesi@hotmail.com>
2021-04-12 15:49:15 +0300

    Add project files.

---
+++ YemERP.DomainLayer/Enums/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YemERP.DomainLayer.Enums
+{
+    public enum Status
+    {   
+        Active=1,
+        Modified=2,
+        Passive=3
+    }
+}
